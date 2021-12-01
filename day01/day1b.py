e755e0318e301447106d190c7ad283fee315e6a1
Leo Battle <leowbattle@gmail.com>
2021-12-01 09:01:31 +0000

    day1b

---
+++ day01/day1b.py
@@ -0,0 +1,8 @@
+import itertools as it
+
+f = open("input")
+nums = list(map(int, f.read().splitlines()))
+
+sums = list(map(lambda x: sum(x), zip(nums, nums[1:], nums[2:])))
+numIncrease = sum(map(lambda x: x[1] > x[0], zip(sums, sums[1:])))
+print(numIncrease)
