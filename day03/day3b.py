820a8e2c1c624c16ca05f7a0fecf4d66f301ce80
Leo Battle <leowbattle@gmail.com>
2021-12-03 16:13:59 +0000

    Refactored day3b code to remove rating-specific code from the main loop

--- day03/day3b.py
+++ day03/day3b.py
@@ -5,18 +5,17 @@ lines = f.read().splitlines()
 width = len(lines[0])
 
 def get_rating(which):
-	pref = '1' if which == "oxygen" else '0'
+	keep_pref = '1' if which == "oxygen" else '0'
+	common_pref = 0 if which == "oxygen" else -1
 
 	nums = lines.copy()
 	for i in range(width):	
 		c = Counter(l[i] for l in nums)
-		if which == "co2":
-			c['0'], c['1'] = c['1'], c['0']
-
+		
 		if c['0'] == c['1']:
-			b = pref
+			b = keep_pref
 		else:
-			b = c.most_common(1)[0][0]
+			b = c.most_common()[common_pref][0]
 		
 		nums = [n for n in nums if n[i] == b]
 
@@ -24,7 +23,7 @@ def get_rating(which):
 			return nums[0]
 
 oxygen = int(get_rating("oxygen"), base=2)
-co2 = int(get_rating("co2"), base=2)
+co2    = int(get_rating("co2"),    base=2)
 
 print(f"oxygen = {oxygen} co2 = {co2}")
 print(f"life = {oxygen * co2}")
