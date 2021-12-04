c73e9dd691b80108065e870e659d95849414750a
Leo Battle <leowbattle@gmail.com>
2021-12-04 15:24:13 +0000

    Added notes to day4

--- day04/day4a.py
+++ day04/day4a.py
@@ -1,3 +1,33 @@
+"""
+In day4a I got my first incorrect answer by trying to be too clever.
+
+In my solution bingo boards are stored as a list of numbers and a 25-bit integer that stores which have been marked (1 bit for each number in a 5x5 board). This means it is possible to detect winners efficiently using bitwise operators. For ease of demonstration I will use a 2x2 board:
+
+31 23
+27 89
+
+nums: [31, 23, 27, 89]
+marked: 0b1000
+
+In order to detect a winner, you check to see if any rows or columns are completely filled in using a bitmask.
+Items in the same row are stored adjacently so the bitmask is 0b1100 for the first row and 0b0011 for the second row. The bitmask for the nth row is (0b11 >> (n * 2)).
+Items in the same column are stored in every nth item in an n-by-n board, in this case every 2nd item, so the bitmask is 0b1010 for the first column and 0b0101 for the second column. The bitmask of the nth column is (0b1010 >> n).
+
+So does the example board win?
+0b1000 & 0b1100 != 0b1100, it doesn't win by the first row.
+0b1000 & 0b0011 != 0b0011, it doesn't win by the second row.
+0b1000 & 0b1010 != 0b1010, it doesn't win by the first column.
+0b1000 & 0b0101 != 0b0101, it doesn't win by the second column.
+
+So the board doesn't win. If marked is changed to 0b1100 then the board wins by the first row.
+
+The reason that I got an incorrect answer is because in `Board.is_winner` I wrote `c = col_win << i`, where it should have been `>>`. I then assumed that if the code produces the correct answer on the example data it will also product the correct answer for the puzzle input. However the example data won using a row, so I didn't realise that the column win testing code was incorrect, and coincidentally my puzzle input won using a column, which my program could not detect.
+
+Interestingly, the incorrect code may have worked anyway because the puzzle input is randomised for each person and I assume that it is a 50-50 chance wether a given puzzle input wins using a row or column. However I would rather get the incorrect answer and fix the program than get a correct answer and unknowingly leave the program broken forever.
+
+Note: Even though it is not required for the challenge, the bitmask method is also capable of detecting diagonal wins, using the bitmasks 0b10000_01000_00100_00010_00001 and 0b00001_00010_00100_01000_10000.
+"""
+
 from dataclasses import dataclass
 from typing import List
 
--- day04/day4b.py
+++ day04/day4b.py
@@ -1,3 +1,9 @@
+"""
+For part 2 you need to find the last board to win, so every time a board wins I add it to the set of all boards that have won. Once the set contains every board, return the last board that was added.
+
+To make that work, a few minor changes had to be made to the rest of the program. In Python, an object can only be added to a set or dict if it is hashable, which dataclasses are not by default. I also couldn't use dataclass's built-in hash function because I only want the hash based off nums, so I wrote my own __hash__. nums had to be changed from List to Tuple because Lists can not be hashed, a tuple is an immutable list that can be hashed.
+"""
+
 from dataclasses import dataclass
 from typing import Tuple
 
