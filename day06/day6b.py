72186e046350c7eeebf2b37bd5211a2e9d137f1f
Leo Battle <leowbattle@gmail.com>
2021-12-06 17:27:50 +0000

    day6b. Note: requires python3.10 for Counter.total

---
+++ day06/day6b.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+f = open("input")
+fish = Counter(map(int, f.read().split(',')))
+
+for i in range(1, 257):
+	fish2 = Counter()
+	for time, count in fish.items():
+		time -= 1
+		if time < 0:
+			time = 6
+			fish2[8] += count
+		fish2[time] += count
+	fish = fish2
+
+print(fish.total())
