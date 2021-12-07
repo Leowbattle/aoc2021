892179e7fb3b3f3d80cab44bf49d58785381b9bc
Leo Battle <leowbattle@gmail.com>
2021-12-07 20:47:39 +0000

    improvement 7b

--- day07/day7b.py
+++ day07/day7b.py
@@ -1,19 +1,13 @@
 f = open("input")
-crabs = sorted(map(int, f.read().split(',')))
+crabs = list(map(int, f.read().split(',')))
 
 # calculate the fuel cost for all crabs to go to position pos
 def cost(pos):
 	total = 0
 	for c in crabs:
 		dist = abs(pos - c)
-		total += dist * (dist + 1) / 2
+		total += dist * (dist + 1) // 2
 	return total
 
-def sign(x):
-	return 1 if x >= 0 else -1
-
-def g(pos):
-	return sum(pos - c + 0.5*sign(pos - c) for c in crabs)
-
-p = -g(0) / len(crabs)
-print(f"approximate O(n) solution: {cost(p)}")
+m = sum(crabs) / len(crabs)
+print(min(cost(int(m + 0.5)), cost(int(m - 0.5))))
